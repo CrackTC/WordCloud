9ee8f39262ffb2379465cc4affc7538e94f5ea2b
CrackTC <chenrke369@gmail.com>
2024-11-01 00:18:06 +0800

    perf: suppress gc transition

--- src/WordCloud.cs
+++ src/WordCloud.cs
@@ -33,9 +33,11 @@ public partial class WordCloud(
     private int GetIndex(int x, int y) => y * width + x;
 
     [LibraryImport("wordcloud", EntryPoint = "cumulative_sum")]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvSuppressGCTransition)])]
     private static partial void CumulativeSum([In, Out] int[] arr, int width, int height);
 
     [LibraryImport("wordcloud", EntryPoint = "hit_count")]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvSuppressGCTransition)])]
     private static partial void HitCount([In, Out] int[] arr, int width, int height, int bw, int bh, [Out] int[] hits);
 
     private SKPoint? GetTextPosition(SKPixmap pixmap, string text, SKPaint paint, bool vertical)
